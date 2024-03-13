15e7e24221672607c731bab4bb3a243a83729b3f
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-13 18:03:43 -0300

    Construção básica do front-end

---
+++ projeto/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>FoodExpress - Login</title>
+    </Head>
+    <div>
+      <h1>FoodExpress</h1>
+    </div>
+    
+    </>
+  );
+}
