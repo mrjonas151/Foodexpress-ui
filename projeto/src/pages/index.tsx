cbf57d8c09164bce3ca6b7df3deab435a649e242
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-15 17:58:32 -0300

    Criação da tela de login e criando componentes reutilizaveis no front

--- projeto/src/pages/index.tsx
+++ projeto/src/pages/index.tsx
@@ -1,4 +1,11 @@
 import Head from "next/head";
+import styles from '../styles/home.module.scss';
+import Image from "next/image";
+
+import logoImg from '../../public/logo.png';
+
+import { Input } from "../components/ui/Input"
+import { Button } from "../components/ui/Button";
 
 export default function Home() {
   return (
@@ -6,8 +13,18 @@ export default function Home() {
     <Head>
       <title>FoodExpress - Login</title>
     </Head>
-    <div>
-      <h1>FoodExpress</h1>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo FoodExpress" />
+
+      <div className={styles.login}>
+        <form className={styles.form}>
+          <Input placeholder="Digite seu e-mail" type="text" />
+          <Input placeholder="Digite sua senha" type="password" />
+          <Button type="submit" loading={true}>Acessar</Button>
+        </form>
+
+        <a className = {styles.text}>Não possui uma conta? Cadastre-se</a>
+      </div>
     </div>
     
     </>
